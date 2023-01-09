f3260b7432296f51f469a950c346dc20026d1bb1
Kaempy <campa.banj@gmail.com>
2023-01-09 16:36:11 +0100

    first commit

---
+++ src/components/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjBR7YawjReb_amZWOmIbb9dddx2rr_XY",
+  authDomain: "react--auth-3f2a0.firebaseapp.com",
+  projectId: "react--auth-3f2a0",
+  storageBucket: "react--auth-3f2a0.appspot.com",
+  messagingSenderId: "25686680923",
+  appId: "1:25686680923:web:599db165cb3e29e970267e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
